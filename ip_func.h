04a4e6aa7a0137d35a966f000a109bef50f78fb2
fuzzler <40913291+fuzzler@users.noreply.github.com>
2020-02-26 18:09:36 +0100

    Add files via upload

---
+++ ip_func.h
@@ -0,0 +1,23 @@
+// Funzioni del progetto IP-Analyzr C++
+
+/*
+ * LISTA DELLE FUNZIONI:
+ * =====================
+ * 
+ * VALID_IP -> verify if IP address (or subnet) is a valid IPv4 format
+*/
+
+// package
+#include<string>
+#include<sstream>
+#include<iostream>
+#include<cstring> // == string.h (nome originale nella libreria di C)
+#include<stdlib.h> // per usare rand e srand
+#include<ctime> // == time.h (nome originale nella libreria di C) => necessario a srand
+#include<cmath> // == math.h (nome originale nella libreria di C)
+#include<ctype.h> // == cctype (per C++)
+#include<iomanip> // per usare setw e setfill
+
+
+using namespace std;
+
